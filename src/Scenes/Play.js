d6585ebc3406b914a406af4eb2a3531fb0b4e407
Daniel Do <do2daniel@yahoo.com>
2023-05-14 04:06:11 -0700

    Working panda movement, random branch spawns

--- src/Scenes/Play.js
+++ src/Scenes/Play.js
@@ -1,98 +1,111 @@
-// Initialize Phaser 3
-const config = {
-    type: Phaser.AUTO,
-    width: 640,
-    height: 480,
-    physics: {
-      default: 'arcade',
-      arcade: {
-        gravity: { y: 0 },
-        debug: false
+class Play extends Phaser.Scene {
+    constructor() {
+      super("playScene");
+    }
+  
+    // Initialize game variables
+    score = 0;
+    scoreText;
+    bamboo;
+    branch;
+    panda;
+    gameover = false;
+    moveSpeed = 2;
+  
+    // Load game assets
+    preload() {
+      this.load.image('bamboo', './assets/bamboo.jpg');
+      this.load.image('branch', './assets/branch.png');
+      this.load.image('background', './assets/background.jpeg');
+      this.load.spritesheet('panda', './assets/panda.png', { frameWidth: 269, frameHeight: 244 });
+    }
+  
+    // Create game objects
+    create() {
+
+      // Add background
+      this.background = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'background').setOrigin(0, 0);
+
+      // Add bamboo to the game world
+      //this.bamboo = new Bamboo(this, (game.config.width / 2) - 50, game.config.height - 300, 'bamboo', 0).setOrigin(0, 0);
+      this.bamboo = this.physics.add.group();
+      this.bamboo.create((game.config.width / 2) - 50, game.config.height - 1200, 'bamboo').setScale(1).setOrigin(0, 0);
+      this.bamboo.enableBody = true;
+      this.bamboo.physicsBodyType = Phaser.Physics.ARCADE;
+  
+      // Add branches to the game world
+      this.branch = this.physics.add.group();
+      this.branch.enableBody = true;
+      this.branch.physicsBodyType = Phaser.Physics.ARCADE;
+
+      // Add panda
+      //this.panda = new Sprite(this, (game.config.width / 4) * 3 - 50, game.config.height - borderUISize - borderPadding, 'panda').setOrigin(0, 0);
+      this.panda = this.physics.add.sprite((game.config.width / 4) * 3 - 60, game.config.height - borderUISize - borderPadding, 'panda').setScale(0.4);
+      this.panda.setCollideWorldBounds(true);
+  
+      // Add score text to the game world
+      this.scoreText = this.add.text(10, 10, 'Score: 0', { font: '32px Arial', fill: '#FFFF00' });
+  
+      // Add keys
+      keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+      keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+  
+      // Add collision detection between panda and branches
+      this.physics.add.collider(this.panda, this.branch, () => {
+        this.gameover = true;
+      });
+
+      this.time.addEvent({
+        delay: 1000, // in milliseconds
+        callback: this.spawnBranch,
+        callbackScope: this,
+        loop: true
+      });
+    }
+
+    spawnBranch() {
+        // Spawn new branches randomly
+        let branchChance = Phaser.Math.Between(1, 2);
+        let branchPosition = Phaser.Math.Between(1, 2);
+        let branchX = 0;
+        if (branchChance == 1) {
+            if (branchPosition == 1) {
+                branchX = (game.config.width / 4) + 60; // spawn on left of bamboo
+            } else {
+                branchX = ((game.config.width / 4) * 3) - 60; // spawn on right of bamboo
+            }
+            this.branch.create(branchX, -32, 'branch');
+        }
+    }
+  
+    // Update game logic
+    update() {
+      // Panda left/right movement
+      if(keyLEFT.isDown) {
+        this.panda.x = (game.config.width / 4) + 60;
+        this.panda.flipX = true;
+      } else if (keyRIGHT.isDown) {
+        this.panda.x = (game.config.width / 4) * 3 - 60;
+        this.panda.flipX = false;
       }
-    },
-    scene: {
-      preload: preload,
-      create: create,
-      update: update
-    }
-  };
+      // Move bamboo and branches down the screen
+      this.bamboo.setVelocityY(200);
+      this.branch.setVelocityY(200);
+
+      // Infinite bamboo length
+      let bambooChance = Phaser.Math.Between(1, 50);
+      if (bambooChance === 1) {
+        this.bamboo.create((game.config.width / 2) - 50, game.config.height - 1200, 'bamboo').setScale(1).setOrigin(0, 0);
+      }
+
+      this.background.tilePositionY -= 4;
   
-  const game = new Phaser.Game(config);
-  
-  // Initialize game variables
-  let score = 0;
-  let scoreText;
-  let tree;
-  let branch;
-  let lumberjack;
-  let gameover = false;
-  
-  // Load game assets
-  function preload() {
-    this.load.image('tree', 'assets/tree.png');
-    this.load.image('branch', 'assets/branch.png');
-    this.load.spritesheet('lumberjack', 'assets/lumberjack.png', { frameWidth: 128, frameHeight: 128 });
-  }
-  
-  // Create game objects
-  function create() {
-    // Add trees to the game world
-    tree = this.physics.add.group();
-    tree.create(320, 0, 'tree').setScale(2).setOrigin(0.5, 0);
-    
-    // Add branches to the game world
-    branch = this.physics.add.group();
-    
-    // Add lumberjack to the game world
-    lumberjack = this.physics.add.sprite(320, 400, 'lumberjack').setScale(0.5);
-    lumberjack.setCollideWorldBounds(true);
-    
-    // Add score text to the game world
-    scoreText = this.add.text(10, 10, 'Score: 0', { font: '32px Arial', fill: '#000000' });
-    
-    // Add keyboard controls
-    this.input.keyboard.on('keydown_LEFT', function (event) {
-      lumberjack.setVelocityX(-200);
-    });
-    this.input.keyboard.on('keyup_LEFT', function (event) {
-      lumberjack.setVelocityX(0);
-    });
-    this.input.keyboard.on('keydown_RIGHT', function (event) {
-      lumberjack.setVelocityX(200);
-    });
-    this.input.keyboard.on('keyup_RIGHT', function (event) {
-      lumberjack.setVelocityX(0);
-    });
-    
-    // Add collision detection between lumberjack and branches
-    this.physics.add.collider(lumberjack, branch, function () {
-      gameover = true;
-    });
-  }
-  
-  // Update game logic
-  function update() {
-    // Move trees and branches down the screen
-    tree.setVelocityY(200);
-    branch.setVelocityY(200);
-    
-    // Spawn new branches randomly
-    let branchChance = Phaser.Math.Between(1, 10);
-    if (branchChance === 1) {
-      let branchX = Phaser.Math.Between(0, 640);
-      branch.create(branchX, -32, 'branch');
-    }
-    
-    // Increment score for every tree chopped
-    this.physics.add.overlap(lumberjack, tree, function () {
-      tree.clear(true, true);
-      score += 1;
-      scoreText.setText('Score: ' + score);
-    });
-    
+      this.scoreText.setText('Score: ' + Math.floor(this.time.now * 0.001));
+
     // Game over logic
-    if (gameover) {
+    if (this.gameover) {
       this.scene.pause();
       this.add.text(200, 200, 'Game Over', { font: '64px Arial', fill: '#FF0000' });
     }
   }
+}
