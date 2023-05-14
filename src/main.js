4534cfa544a759ef8ca52bb0aa05905f682971bd
Daniel Do <do2daniel@yahoo.com>
2023-05-14 06:30:32 -0700

    Added header comment

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
+/*
+Daniel Do
+"Panda Climb"
+~38 hours
+
+Creative tilt justification:
+50/50 chance of spawning a bamboo branch every second
+Learned Arcade Physics: gravity, velocity changes, sprite creations
+Jenky way of making it feel like the bamboo has an infinite length
+Flipped panda sprite along the center of the canvas (calculating equal fractions of the canvas)
+Learned how to use Aesprite to make the art for the panda, bamboo, and its branches
+Unintentional paralax between the main bamboo sprite and the scrolling bamboo background
+*/
+
 let config = {
     type: Phaser.AUTO,
     width: 640,
